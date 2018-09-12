a04535db90d8ea7954e94baa2d6b9c92db03ff14
kennetholvr <kennetholvr@gmail.com>
2018-09-12 10:04:22 -0500

    moved server file and started proper auth

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const Sequelize = require('sequelize')
-const epilogue = require('epilogue')
+// const Sequelize = require('sequelize')
+// const epilogue = require('epilogue')
 const OktaJwtVerifier = require('@okta/jwt-verifier')
 
 const oktaJwtVerifier = new OktaJwtVerifier({
---
+++ src/components/login.vue
@@ -0,0 +1,2 @@
+<temmplate>
+</template>
